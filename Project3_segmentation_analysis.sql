e0e56a961aa5e58c1dfefc34f8ea1b1ce883dd65
minhnbnguyen <142035875+minhnbnguyen@users.noreply.github.com>
2024-10-14 15:41:57 -0500

    Update Project3_segmentation_analysis.sql

--- Project3_segmentation_analysis.sql
+++ Project3_segmentation_analysis.sql
@@ -51,7 +51,7 @@ UPDATE sales_dataset_rfm_prj
 SET contactfirstname = SUBSTRING (contactfullname FROM POSITION ('-' IN contactfullname)+1);
 
 UPDATE sales_dataset_rfm_prj
-SET contactlastname = (contactfullname FROM 1 FOR POSITION ('-' IN contactfullname)-1);
+SET contactlastname = SUBSTRING (contactfullname FROM POSITION ('-' IN contactfullname)-1);
 
 UPDATE sales_dataset_rfm_prj
 SET contactfirstname= CONCAT (UPPER (LEFT (contactfirstname,1)),LOWER (RIGHT (contactfirstname,(LENGTH (contactfirstname)-1))));
@@ -82,47 +82,71 @@ SELECT
 percentile_cont (0.25) WITHIN GROUP (ORDER BY quantityordered) AS Q1,
 percentile_cont (0.75) WITHIN GROUP (ORDER BY quantityordered) AS Q3,
 percentile_cont (0.75) WITHIN GROUP (ORDER BY quantityordered) - percentile_cont (0.25) WITHIN GROUP (ORDER BY quantityordered) AS IQR
-FROM sales_dataset_rfm_prj)
+FROM sales_dataset_rfm_prj),
+outlier AS (
 SELECT
 *
 FROM sales_dataset_rfm_prj
 WHERE quantityordered < (SELECT MIN (Q1 - 1.5*IQR) FROM a)
 OR quantityordered > (SELECT MAX (Q3 + 1.5*IQR) FROM a)
+)
 -- Step 6: Clean the outlier
-DELETE FROM TABLE sales_dataset_rfm_prj
+DELETE FROM sales_dataset_rfm_prj
 WHERE quantityordered IN (SELECT quantityordered FROM outlier);
+
 -- Begin Analysis
 -- Which month has the highest revenue?
-SELECT month_id,
-SUM (sales) AS revenue,
-ordernumber AS order_number
-FROM sales_dataset_rfm_prj
-GROUP BY month_id, ordernumber
+SELECT
+	month_id,
+	SUM (sales) AS revenue,
+	ordernumber AS order_number
+FROM
+	sales_dataset_rfm_prj
+GROUP BY
+	month_id, ordernumber
 -- Which productline has the most sales in november?
-SELECT month_id,
-SUM (sales) AS revenue,
-ordernumber AS order_number
-FROM sales_dataset_rfm_prj
-WHERE month_id = 11
-GROUP BY month_id, ordernumber
-ORDER BY SUM (sales) DESC)
+SELECT
+	month_id,
+	SUM (sales) AS revenue,
+	ordernumber AS order_number
+FROM
+	sales_dataset_rfm_prj
+WHERE
+	month_id = 11
+GROUP BY
+	month_id, ordernumber
+ORDER BY
+	SUM (sales) DESC
 -- Revenue of each productline, year and dealsize
-SELECT productline, year_id, dealsize,
-SUM (sales) AS revenue
-FROM sales_dataset_rfm_prj
-GROUP BY productline, year_id, dealsize
+SELECT
+	productline, year_id, dealsize,
+	SUM (sales) AS revenue
+FROM
+	sales_dataset_rfm_prj
+GROUP BY
+	productline, year_id, dealsize
 -- Which product has the highes revenue in the UK each year
-SELECT *
-FROM (
-SELECT *,
-DENSE_RANK () OVER (PARTITION BY year_id ORDER BY revenue) AS RANK
-FROM (
-SELECT year_id, productline, SUM (sales) AS revenue
-FROM sales_dataset_rfm_prj
-WHERE country = 'UK'
-GROUP BY year_id, productline, country)
-ORDER BY RANK () OVER (PARTITION BY year_id, productline ORDER BY revenue))
-WHERE RANK = 1
+SELECT
+	*
+FROM
+(
+	SELECT
+	*,
+	DENSE_RANK () OVER (PARTITION BY year_id ORDER BY revenue) AS RANK
+	FROM
+	(
+	SELECT
+		year_id, productline, SUM (sales) AS revenue
+	FROM
+		sales_dataset_rfm_prj
+	WHERE
+		country = 'UK'
+	GROUP BY
+		year_id, productline, country)
+	ORDER BY
+		RANK () OVER (PARTITION BY year_id, productline ORDER BY revenue))
+	WHERE
+		RANK = 1
 -- Who is the best customer according to RFM
 -- Input segmentation score table
 CREATE TABLE segment_score
@@ -130,24 +154,37 @@ CREATE TABLE segment_score
     segment Varchar,
     scores Varchar)
 -- Calculate Recency, Frequency and Monetary
-WITH rfm AS (
-SELECT customername,
-current_date - MAX(orderdate) AS R,
-COUNT (DISTINCT ordernumber) AS F,
-SUM (sales) AS M
-FROM sales_dataset_rfm_prj
-GROUP BY customername),
+WITH rfm AS
+(
+	SELECT
+		customername,
+		current_date - MAX(orderdate) AS R,
+		COUNT (DISTINCT ordernumber) AS F,
+		SUM (sales) AS M
+	FROM
+		sales_dataset_rfm_prj
+	GROUP BY
+	customername
+),
 -- Divide customers into groups
-segmentation_score AS (
-SELECT customername,
-ntile (5) OVER (ORDER BY R DESC) AS r_score,
-ntile (5) OVER (ORDER BY F) AS f_score,
-ntile (5) OVER (ORDER BY M) AS m_score
-FROM rfm)
-SELECT a.*, b.segment
-FROM (
-SELECT customername,
-CAST (r_score AS VARCHAR)|| CAST (f_score AS VARCHAR) || CAST (m_score AS VARCHAR) AS seg_score
-FROM segmentation_score) AS a
-JOIN segment_score AS b ON a.seg_score=b.scores
+segmentation_score AS
+(
+	SELECT
+	customername,
+	ntile (5) OVER (ORDER BY R DESC) AS r_score,
+	ntile (5) OVER (ORDER BY F) AS f_score,
+	ntile (5) OVER (ORDER BY M) AS m_score
+FROM
+	rfm
+)
+SELECT
+	a.*, b.segment
+FROM
+(
+	SELECT
+		customername,
+	CAST (r_score AS VARCHAR)|| CAST (f_score AS VARCHAR) || CAST (m_score AS VARCHAR) AS seg_score
+	FROM
+		segmentation_score) AS a
+	JOIN segment_score AS b ON a.seg_score=b.scores
 -- Visualization & business insights with Tableau -> Check Project 2 visualization
